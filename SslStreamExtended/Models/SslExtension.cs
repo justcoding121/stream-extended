66ef543569b60aace809f6cd2a0224ccb1e0f2b7
justcoding121 <jehonathant@gmail.com>
2017-06-29 22:31:22 -0400

    Initial commit

---
+++ SslStreamExtended/Models/SslExtension.cs
@@ -0,0 +1,18 @@
+namespace SslStreamExtended.Models
+{
+    public class SslExtension
+    {
+        public int Value { get; set; }
+
+        public string Name { get; set; }
+
+        public string Data { get; set; }
+
+        public SslExtension(int value, string name, string data)
+        {
+            Value = value;
+            Name = name;
+            Data = data;
+        }
+    }
+}
