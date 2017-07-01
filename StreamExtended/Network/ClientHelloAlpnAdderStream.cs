17bc74ca02d5e7e122c5f6ccfc43f11e74f6d821
justcoding121 <jehonathant@gmail.com>
2017-07-01 06:50:19 -0400

    expose CustomBufferedStream

--- StreamExtended/Network/ClientHelloAlpnAdderStream.cs
+++ StreamExtended/Network/ClientHelloAlpnAdderStream.cs
@@ -9,9 +9,9 @@ namespace StreamExtended.Network
 
         private bool called;
 
-        public ClientHelloAlpnAdderStream(Stream stream, int bufferSize = 4096)
+        public ClientHelloAlpnAdderStream(CustomBufferedStream stream)
         {
-            this.stream = new CustomBufferedStream(stream, bufferSize);
+            this.stream = stream;
         }
 
         public override void Flush()
