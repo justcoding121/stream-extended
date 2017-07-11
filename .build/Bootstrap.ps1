e6efa5083145080ef4e09c4e98d9f4b97c8411a4
justcoding121 <jehonathant@gmail.com>
2017-07-10 20:01:26 -0400

    exp fix

--- .build/Bootstrap.ps1
+++ .build/Bootstrap.ps1
@@ -6,6 +6,15 @@ param (
 
 $Here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+
+$MSBuild  = exec { . $Here\vswhere.exe -latest -products * -requires Microsoft.Component.MSBuild -property installationPath }
+if ($MSBuild) {
+  $MSBuild  = join-path $MSBuild 'MSBuild\15.0\Bin\MSBuild.exe'
+}
+
+Write-Host "$MSBuild"
+
+
 Import-Module "$Here\Common"
 
 Install-Chocolatey
@@ -14,7 +23,10 @@ Install-Psake
 
 $psakeDirectory = (Resolve-Path $env:ChocolateyInstall\lib\Psake*)
 
-Import-Module (Join-Path $psakeDirectory "tools\Psake.psm1")
+ if(!(Test-Path $psakeDirectory)) 
+ {
+    Import-Module (Join-Path $psakeDirectory "tools\Psake.psm1")
+ }
 
 if($Help)
 { 
