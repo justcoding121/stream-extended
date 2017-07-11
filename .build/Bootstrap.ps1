138d89b601319a1c99d246017747c90aabc05da9
justcoding121 <jehonathant@gmail.com>
2017-07-10 20:11:51 -0400

    exp test build

--- .build/Bootstrap.ps1
+++ .build/Bootstrap.ps1
@@ -7,37 +7,10 @@ param (
 $Here = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 
-$MSBuild  = exec { . $Here\vswhere.exe -latest -products * -requires Microsoft.Component.MSBuild -property installationPath }
+$MSBuild  = & $Here\vswhere.exe -latest -products * -requires Microsoft.Component.MSBuild -property installationPath 
 if ($MSBuild) {
   $MSBuild  = join-path $MSBuild 'MSBuild\15.0\Bin\MSBuild.exe'
 }
 
 Write-Host "$MSBuild"
 
-
-Import-Module "$Here\Common"
-
-Install-Chocolatey
-
-Install-Psake
-
-$psakeDirectory = (Resolve-Path $env:ChocolateyInstall\lib\Psake*)
-
- if(!(Test-Path $psakeDirectory)) 
- {
-    Import-Module (Join-Path $psakeDirectory "tools\Psake.psm1")
- }
-
-if($Help)
-{ 
-	try 
-	{
-		Write-Host "Available build tasks:"
-		psake -nologo -docs | Out-Host -paging
-	} 
-	catch {}
-
-	return
-}
-
-Invoke-Psake -buildFile "$Here\Default.ps1" -parameters $properties -tasklist $Action
